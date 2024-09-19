39ba738c342b2e596c964bf39ba08456ac6560fa
zhengzihao1998 <zhengzihao27@163.com>
2024-09-19 15:23:20 +0800

    2.1

---
+++ javascripts/mathjax.js
@@ -0,0 +1,26 @@
+window.MathJax = {
+    loader: {load: ['[tex]/boldsymbol']},
+
+    tex: {
+      inlineMath: [["\\(", "\\)"]],
+      displayMath: [["\\[", "\\]"]],
+      processEscapes: true,
+      processEnvironments: true,
+
+      // 自动方程编号
+      tags: 'ams',
+
+      packages: {'[+]': ['boldsymbol']}
+    },
+    options: {
+      ignoreHtmlClass: ".*|",
+      processHtmlClass: "arithmatex"
+    }
+  };
+  
+  document$.subscribe(() => { 
+    MathJax.startup.output.clearCache()
+    MathJax.typesetClear()
+    MathJax.texReset()
+    MathJax.typesetPromise()
+  })
